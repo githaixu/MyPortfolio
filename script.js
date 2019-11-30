a4580fa85951b3422119cb3834d9874dfbceadad
Jhin <40558041+githaixu@users.noreply.github.com>
2019-11-30 13:40:14 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.querySelector('.open').addEventListener('click', ()=>{
+  document.querySelector('.nav-list').classList.add('active');
+})
+document.querySelector('.close').addEventListener('click', ()=>{
+  document.querySelector('.nav-list').classList.remove('active');
+})
+function clickPageLink(){
+    document.querySelector('.nav-list').classList.remove('active');
+}
